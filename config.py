7e03d344b109ea79b65a3de0ede0a0dd853fb448
ACuteWoof <sutharshanvithushan@gmail.com>
2021-05-16 12:43:20 +0530

    QTILE

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
-
+from libqtile import qtile
 
 lazy.to_screen(1)
 lazy.spawn("picom")
