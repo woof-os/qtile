08fe6903fdfba661bc8cbe39bf049833d9ac9a45
ACuteWoof <sutharshanvithushan@gmail.com>
2024-06-11 19:01:23 +0530

    config

--- config.py
+++ config.py
@@ -88,11 +88,13 @@ keys = [
         [mod], "v", lazy.spawn("rofi -show window"), desc="Show active windows in rofi"
     ),
     Key([mod], "f", lazy.spawn("flameshot gui"), desc="Open flameshot gui"),
-    Key([mod], "s", lazy.spawn("scrot"), desc="Take full screen ss using scrot"),
+    Key([mod], "e", lazy.spawn("tauon"), desc="Open tauon music box"),
     Key([mod], "z", lazy.spawn(f"{home}/.config/qtile/view.sh"), desc="View in Zathura"),
     Key([mod], "y", lazy.spawn(f"{home}/.config/qtile/yt.sh"), desc="Open YouTube video in MPV"),
     Key([mod], "g", lazy.spawn("rofi -show filebrowser"), desc="Open YouTube video in MPV"),
     Key([mod, "control"], "p", lazy.spawn("rofi -show power-menu -modi power-menu:rofi-power-menu"), desc="Power Menu"),
+    Key([mod], "a", lazy.spawn("brave notion.so"), desc="Open notion"),
+    Key([mod], "s", lazy.spawn("brave esv.org"), desc="Open ESV"),
     # Toggle between screens
     Key([mod], 'period', lazy.next_screen(), desc='Next display'),
 ]
